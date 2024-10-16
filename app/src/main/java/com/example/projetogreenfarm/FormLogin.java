2ba949839c8212acd413690ae1144ff1dd0d0a2e
NicolasAndrioli <Razachai@outlook.com>
2024-10-16 02:03:54 -0300

    criação da tela de cadastro

--- app/src/main/java/com/example/projetogreenfarm/FormLogin.java
+++ app/src/main/java/com/example/projetogreenfarm/FormLogin.java
@@ -1,24 +1,109 @@
 package com.example.projetogreenfarm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.ActionBar;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class FormLogin extends AppCompatActivity {
+
+
+    private TextView text_cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_form_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Verifica se a ActionBar existe antes de tentar escondê-la
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        IniciarComponentes();
+
+        text_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FormLogin.this,FormCadastro.class);
+                startActivity(intent);
+
+
+            }
         });
     }
+
+    private void IniciarComponentes(){
+        text_cadastrar = findViewById(R.id.text_cadastrar);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
