99174999e3289bf2d3ff233e629eb772393aab42
NicolasAndrioli <Razachai@outlook.com>
2024-10-22 22:46:24 -0300

    Projeto Finalizado, feita a integração do firebase com a recuperação de senha

--- app/src/main/java/com/example/projetogreenfarm/FormRedefinirSenha.java
+++ app/src/main/java/com/example/projetogreenfarm/FormRedefinirSenha.java
@@ -1,6 +1,10 @@
 package com.example.projetogreenfarm;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,76 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FormRedefinirSenha extends AppCompatActivity {
+
+    private Button bt_linkderecuperação;
+    private EditText emailRecuperacao;
+    String[] mensagens = {"Preencha o campo com um e-mail válido","Se o e-mail estiver registrado, enviaremos um link para redefinição de senha"};
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_form_redefinir_senha);
+
+        IniciarComponentes();
+
+        auth = FirebaseAuth.getInstance();
+
+        bt_linkderecuperação.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailRecuperacao.getText().toString();
+
+                if(email.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view,mensagens[0],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }else{
+                    auth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Snackbar snackbar = Snackbar.make(view, mensagens[1], Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.WHITE);
+                            snackbar.setTextColor(Color.BLACK);
+                            snackbar.show();
+                        } else {
+                            Snackbar snackbar = Snackbar.make(view, mensagens[2], Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.RED);
+                            snackbar.setTextColor(Color.WHITE);
+                            snackbar.show();
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+
+
+
+
+
+
+        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void IniciarComponentes(){
+        bt_linkderecuperação = findViewById(R.id.bt_linkderecuperação);
+        emailRecuperacao = findViewById(R.id.emailRecuperacao);
+
+
+
+
     }
 }
